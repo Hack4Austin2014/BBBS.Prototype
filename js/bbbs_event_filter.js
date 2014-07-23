38ce576f193854fae1b5749d13c69d6c2bedd645
IVxIV <software@ivxiv.com>
2014-07-22 23:45:16 -0500

    add filter by cost
    javascript filtering of events by cost

--- js/bbbs_event_filter.js
+++ js/bbbs_event_filter.js
@@ -15,7 +15,7 @@ function bbbs_event_filter()
 	// filter events by category
 	// in_events: array of BBBS event objects
 	// in_categories: array of categories (strings)
-	// returns BBBS events from in_events whose category matches one of the entries in in_categories
+	// returns BBBS events from in_events whose category matches one of the entries in in_categories, or null
 	this.filter_by_category= function(in_events, in_categories)
 	{
 		var result= null;
@@ -44,6 +44,7 @@ function bbbs_event_filter()
 				{
 					// invalid category - remove it
 					categories.splice(i, 1);
+					i-= 1;
 				}
 			}
 			
@@ -70,9 +71,95 @@ function bbbs_event_filter()
 				else
 				{
 					console.log("bbbs_event_filter.filter_by_category(): in_event[" + i + "] has no 'category' field!");
+					console.log(in_event[i]);
 				}
 				
-				if (true==remove_event)
+				if (Boolean(remove_event))
+				{
+					result.splice(i, 1);
+					i-= 1;
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	// filter events by cost
+	// in_events: array of BBBS event objects
+	// in_costs: array of costs (strings, expected to look like '$$$$', '$$$', '$$' or '$')
+	// returns BBBS events from in_events whose cost matches one of the entries in in_costs, or null
+	this.filter_by_cost= function(in_events, in_costs)
+	{
+		var result= null;
+		
+		if (!(in_events instanceof Array))
+		{
+			console.log("bbbs_event_filter.filter_by_cost(): in_events is not an Array!");
+		}
+		else if (!(in_costs instanceof Array))
+		{
+			console.log("bbbs_event_filter.filter_by_cost(): in_cost is not an Array!");
+		}
+		else
+		{
+			var valid_costs= ["$$$$", "$$$", "$$", "$"];
+			var costs= in_costs.splice(0);
+			var i, j;
+			
+			// sanitize costs
+			for (i= 0; i<costs.length; i++)
+			{
+				var remove_cost= true;
+				
+				if ((null != costs[i]) && ("string" == typeof costs[i]))
+				{
+					for (j= 0; j<valid_costs.length; j++)
+					{
+						if (valid_costs[j]==costs[i].substr(0, valid_costs[j].length))
+						{
+							costs[i]= valid_costs[j];
+							remove_cost= false;
+							break;
+						}
+					}
+				}
+				
+				if (Boolean(remove_cost))
+				{
+					// invalid cost - remove it
+					console.log("bbbs_event_filter.filter_by_cost(): in_cost '" + costs[i] + "' is invalid!");
+					costs.splice(i, 1);
+					i-= 1;
+				}
+			}
+			
+			result= in_events.slice(0);
+			
+			for (i= 0; i<result.length; i++)
+			{
+				var remove_event= true;
+				
+				if ((null != result[i].pricerange) && ("string" == typeof result[i].pricerange))
+				{
+					var test_cost= new String(result[i].pricerange);
+					
+					for (j= 0; j<costs.length; j++)
+					{
+						if (costs[j]==test_cost.substr(0, costs[j].length))
+						{
+							remove_event= false;
+							break;
+						}
+					}
+				}
+				else
+				{
+					console.log("bbbs_event_filter.filter_by_cost(): in_event[" + i + "] has no 'pricerange' field!");
+					console.log(in_event[i]);
+				}
+				
+				if (Boolean(remove_event))
 				{
 					result.splice(i, 1);
 					i-= 1;
